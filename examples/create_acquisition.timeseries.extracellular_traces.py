c8ad14a5c9f76c80b434e4f264c58b0f887b42b0
denisovg <denisovg@hhmi.org>
2017-03-30 15:39:23 -0400

    Added a folder 'examples' which will provide simple examples of creating partial NWB files

---
+++ examples/create_acquisition.timeseries.extracellular_traces.py
@@ -0,0 +1,2 @@
+dict = {"acquisition.timeseries.extracellular_traces" : "my_extracellular_traces"}
+libdict2nwb.make_partial_nwb("acquisition.timeseries.extracellular_traces", dict, "My_project")
---
+++ examples/set_imports.py
@@ -0,0 +1,5 @@
+import nwb
+from nwb import nwb_file
+from nwb import nwb_utils
+import h5py
+import libdict2nwb
