07a3b0fffeaeac70a0848a52694a79831d3d5f49
denisovg <denisovg@hhmi.org>
2017-04-07 09:21:48 -0400

    Removing the files that are no longer needed

--- examples/create_acquisition.timeseries.extracellular_traces.py
+++
@@ -1,2 +0,0 @@
-dict = {"acquisition.timeseries.extracellular_traces" : "my_extracellular_traces"}
-libdict2nwb.make_partial_nwb("acquisition.timeseries.extracellular_traces", dict, "My_project")
--- examples/set_imports.py
+++
@@ -1,5 +0,0 @@
-import nwb
-from nwb import nwb_file
-from nwb import nwb_utils
-import h5py
-import libdict2nwb
